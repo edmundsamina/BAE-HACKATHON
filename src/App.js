21d5843f07ff3fb4f6d2681d73033d32d7c279be
edmundsamina <edmundsamina@gmail.com>
2023-01-25 11:08:05 +0000

    project init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import character from './images/crayon-man-with-sword.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Character">Your Carbon Warrior
+    <img src={character} alt="character"/></div>
+    <div className="Stats">
+      <input/>
+      <div className="transport-type">
+                <label className="transport"> Transport-Type: </label>
+                <select
+                 
+                >
+                  <option value="Taxi">Taxi</option>
+                  <option value="ClassicBus">ClassicBus</option>
+                  <option value="EcoBus">EcoBus</option>
+                  <option value="Coach">Coach</option>
+                  <option value="NationalTrain">NationalTrain</option>
+                  <option value="LightRail">LightRail</option>
+                  <option value="Subway">Subway</option>
+                  <option value="FerryOnFoot">FerryOnFoot</option>
+                  <option value="FerryInCar">FerryInCar</option>
+                </select>
+              </div>
+      <button>Get Your Carbon Footprint</button>
+    </div>
     </div>
   );
 }
